7b67af98072ea9c67b1d76cdccc0ec37c40efa2a
Ahafid <ahafidkabir11@gmail.com>
2024-03-24 12:32:36 +0600

    Add files via upload

---
+++ VeriableDetatype/src/veriabledetatype/VeriableDetatype.java
@@ -0,0 +1,24 @@
+
+package veriabledetatype;
+public class VeriableDetatype {
+  
+    public static void main(String[] args) {
+        int x = 3;
+        //dataype primitive , non primitive
+        //primitive = boolean , numeric 
+        //numeric=char , integral = integer(byte,short,int,long), floating(float ,double)
+        //nin-primitive = string ,array 
+        double y = 2.333;//dynamic initialization
+        float f ;
+        f= 4f;
+        boolean b = true;
+        System.out.println(x+2);
+        System.out.println(y);
+        System.out.println("value of b = "+b);
+        System.out.println("value of f = "+f);
+        
+       System.out.println("What ");
+       System.out.print("Ahafid ");
+    }
+    
+}
